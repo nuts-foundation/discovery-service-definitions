4f2596d1a4c2c0585c9e3f94af8759b007c877d2
Rein Krul <info@reinkrul.nl>
2023-12-08 13:25:43 +0100

    add update script

---
+++ definitions/update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+BASEDIR=$(dirname "$0")
+# curl flags taken from https://blog.stigok.com/2017/09/18/curl-only-write-to-file-if-successful-status-200.html
+curl -s -S -f -o $BASEDIR/dev_eOverdracht2023.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_eOverdracht2023.json
