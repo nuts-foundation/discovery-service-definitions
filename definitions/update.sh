f7de5ab8f15731383835a53669805c8c43d773fb
Rein Krul <info@reinkrul.nl>
2024-04-11 21:24:22 +0200

    Short-lived URA HomeMonitoring DS

--- definitions/update.sh
+++ definitions/update.sh
@@ -3,3 +3,4 @@ BASEDIR=$(dirname "$0")
 # curl flags taken from https://blog.stigok.com/2017/09/18/curl-only-write-to-file-if-successful-status-200.html
 curl -s -S -f -o $BASEDIR/dev_eOverdracht2023.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_eOverdracht2023.json
 curl -s -S -f -o $BASEDIR/dev_homemonitoring2024.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_homemonitoring2024.json
+curl -s -S -f -o $BASEDIR/dev_homemonitoring_ura2024.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_homemonitoring_ura2024.json
