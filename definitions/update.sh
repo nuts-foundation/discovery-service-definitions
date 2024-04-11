d588f6a4a5fcb07e01177ab17e7e47490c0b0589
Rein Krul <info@reinkrul.nl>
2024-04-11 21:22:29 +0200

    Add URA credential to HomeMonitoring Presentation Definition

--- definitions/update.sh
+++ definitions/update.sh
@@ -2,3 +2,4 @@
 BASEDIR=$(dirname "$0")
 # curl flags taken from https://blog.stigok.com/2017/09/18/curl-only-write-to-file-if-successful-status-200.html
 curl -s -S -f -o $BASEDIR/dev_eOverdracht2023.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_eOverdracht2023.json
+curl -s -S -f -o $BASEDIR/dev_homemonitoring2024.json https://raw.githubusercontent.com/nuts-foundation/discovery-service-definitions/main/definitions/dev_homemonitoring2024.json
